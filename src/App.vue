d6e2ace49c43823d69ea027b1f45c6009311e665
Asl04 <ganiuly.aslan@gmail.com>
2019-02-25 17:09:08 +0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -189,7 +189,39 @@ input:disabled + label:before {
  background: #eee;
  color: #aaa;
 }
+.sort{
+    padding: 0 2%;
+    display: flex;
+    justify-content: space-between;
+}
+.sort-price{
+    width: 256px;
+    height: 40px;
+    background-color: #f0f4f7;
+    border-radius: 8px;
+    border: 1px solid #ccc;
+    margin-top: 10px;
+    font-size: 16px;
+}
+option {
+    background-color: #f0f4f7;
+    border-radius: 8px;
+    border: 1px solid #ccc;
+    }
+select {
+  padding: 5px 35px 5px 5px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  height: 34px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background: url('../../public/images/td_d_sort_arrow.png') 96% / 10% no-repeat #eee;
+}
 
+select::-ms-expand { 
+    display: none; 
+}
 @media only screen and (min-width : 1220px) {
   .flex-container{
    width: 337px;
